7ba77af7338b54dfb5695979152a9a3e7f78d492
FooDog <andyriffic@gmail.com>
2022-09-19 20:07:03 +1000

    Update secrets format in manfest to try to get working

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,5 +3,6 @@ export default async function handler(req: any, res: any) {
   res.json({
     message: "Hello Everyone :)",
     secret: process.env.TEST_SUPER_SECRET || "not found",
+    secret2: process.env.SECRET_TEST_2 || "not found",
   });
 }
