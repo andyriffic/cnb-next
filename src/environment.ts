5d3f21c3718e16103ae64d4182ea33ac3ef4542f
FooDog <andyriffic@gmail.com>
2022-09-20 08:02:58 +1000

    Update some environment variables

--- src/environment.ts
+++ src/environment.ts
@@ -7,6 +7,8 @@ export const GRAPHQL_ENDPOINT: string =
 export const DB_TABLE_NAME_PLAYERS: string =
   process.env.DB_TABLE_NAME_PLAYERS || "";
 
+export const ENVIRONMENT_NAME: string = process.env.ENVIRONMENT_NAME || "";
+
 //private
 export const DYNAMO_DB_ACCESS_KEY = process.env.DYNAMO_DB_ACCESS_KEY || "";
 export const DYNAMO_DB_ACCESS_KEY_SECRET =
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,9 @@
+import { ENVIRONMENT_NAME } from "../../environment";
+
 export default async function handler(req: any, res: any) {
   // Rest of the API logic
   res.json({
     message: "Hello Everyone :)",
-    secret: process.env.TEST_SUPER_SECRET || "not found",
-    secret2: process.env.SECRET_TEST_2 || "not found",
+    environment: ENVIRONMENT_NAME,
   });
 }
