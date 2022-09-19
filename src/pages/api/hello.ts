f59a157e0942250465b10a6a89b0c55efb133dd7
FooDog <andyriffic@gmail.com>
2022-09-19 19:16:48 +1000

    Update name of test secret

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,6 +2,6 @@ export default async function handler(req: any, res: any) {
   // Rest of the API logic
   res.json({
     message: "Hello Everyone :)",
-    secret: process.env.SUPER_SECRET || "not found",
+    secret: process.env.TEST_SUPER_SECRET || "not found",
   });
 }
