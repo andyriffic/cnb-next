0d5d6b97a230260dc38f5937e969379b65fbce99
FooDog <andyriffic@gmail.com>
2022-10-25 17:51:16 +1100

    Get round resolution and validation working for two players making a valid move

--- src/services/games/rockPaperScissors/index.fp.ts
+++ src/services/games/rockPaperScissors/index.fp.ts
@@ -1,6 +1,6 @@
 import * as E from "fp-ts/Either";
-import * as O from "fp-ts/Option";
 import * as A from "fp-ts/Array";
+import * as O from "fp-ts/Option";
 import { pipe } from "fp-ts/function";
 import { RPSCreateGameProps, RPSGame, RPSPlayerMove, RPSRound } from "./types";
 
@@ -34,6 +34,75 @@ export function makePlayerMove(
   );
 }
 
+export function resolveRound(game: RPSGame): E.Either<string, RPSGame> {
+  return pipe(
+    game.rounds,
+    A.last,
+    E.fromOption(() => "No round to resolve"),
+    E.chain(validateRoundCanBeResolved),
+    E.chain(addResultToRound),
+    E.map((round) => updateRoundInGame(round, game))
+  );
+}
+
+function addResultToRound(
+  validatedRound: RPSRound
+): E.Either<string, RPSRound> {
+  const p1Move = validatedRound.moves[0]!;
+  const p2Move = validatedRound.moves[1]!;
+
+  if (p1Move.moveName === p2Move.moveName) {
+    return E.right({ ...validatedRound, result: { draw: true } });
+  }
+
+  switch (p1Move.moveName) {
+    case "rock":
+      return p2Move.moveName === "scissors"
+        ? E.right({
+            ...validatedRound,
+            result: { draw: false, winningPlayerId: p1Move.playerId },
+          })
+        : E.right({
+            ...validatedRound,
+            result: { draw: false, winningPlayerId: p2Move.playerId },
+          });
+    case "paper":
+      return p2Move.moveName === "rock"
+        ? E.right({
+            ...validatedRound,
+            result: { draw: false, winningPlayerId: p1Move.playerId },
+          })
+        : E.right({
+            ...validatedRound,
+            result: { draw: false, winningPlayerId: p2Move.playerId },
+          });
+    case "scissors":
+      return p2Move.moveName === "paper"
+        ? E.right({
+            ...validatedRound,
+            result: { draw: false, winningPlayerId: p1Move.playerId },
+          })
+        : E.right({
+            ...validatedRound,
+            result: { draw: false, winningPlayerId: p2Move.playerId },
+          });
+  }
+}
+
+function validateRoundCanBeResolved(
+  round: RPSRound
+): E.Either<string, RPSRound> {
+  if (round.moves.length < 2) {
+    return E.left("Not all players have moved");
+  }
+
+  if (round.result) {
+    return E.left("Round already has a result");
+  }
+
+  return E.right(round);
+}
+
 function validatePlayerInGame(
   playerId: string,
   game: RPSGame
@@ -42,6 +111,7 @@ function validatePlayerInGame(
     ? E.right(game)
     : E.left(`Player "${playerId}" is not in this game`);
 }
+
 function validatePlayerNotMovedInRound(
   playerId: string,
   round: RPSRound
@@ -55,10 +125,12 @@ function addMoveToRound(round: RPSRound, move: RPSPlayerMove): RPSRound {
   return { ...round, moves: [...round.moves, move] };
 }
 
-function updateRoundInGame(round: RPSRound, game: RPSGame): RPSGame {
+function updateRoundInGame(updatedRound: RPSRound, game: RPSGame): RPSGame {
   return {
     ...game,
-    rounds: game.rounds.map((r) => (r.index === round.index ? round : r)),
+    rounds: game.rounds.map((r) =>
+      r.index === updatedRound.index ? updatedRound : r
+    ),
   };
 }
 
