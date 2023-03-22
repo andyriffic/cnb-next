341d656b6df21dd74e483e3c1da5f4ad69139342
FooDog <andyriffic@gmail.com>
2023-03-22 18:06:04 +1100

    Add finx mascot to default page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,40 @@
 import type { NextPage } from "next";
 import styled from "styled-components";
 import Head from "next/head";
+import Image from "next/image";
+import { CenterSpaced } from "../components/Layouts";
+import { Heading } from "../components/Atoms";
 
-const Heading = styled.h1`
-  color: red;
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  width: 30vw;
+  height: 30vw;
+  position: relative;
 `;
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <Container>
       <Head>
-        <title>Create Next App</title>
+        <title>Finx Rocks!</title>
       </Head>
-
-      <Heading>Hello</Heading>
-    </div>
+      <ImageContainer>
+        <Image
+          src="/images/finx-rocks-mascot.png"
+          layout="fill"
+          alt="Finx Rocks mascot of a rock smiling and using a laptop"
+        />
+      </ImageContainer>
+      <Heading>Finx Rocks!</Heading>
+    </Container>
   );
 };
 
