9f514145335f9085a9cbc2a14af477c82c77bd4c
FooDog <andyriffic@gmail.com>
2023-06-24 12:10:36 +1000

    Fix aoverlord test

--- src/services/ai-overlord/index.test.ts
+++ src/services/ai-overlord/index.test.ts
@@ -6,10 +6,12 @@ const stubAiCreator: AiOverlordCreator = () =>
   TE.right({
     introduction: { english: "Hi", chinese: "" },
     battles: [],
+    initialised: false,
+    moves: [],
   } as AiOverlord);
 
 test("Can create game successfully", async () => {
-  const game = await createAiOverlordGame(stubAiCreator, [])();
+  const game = await createAiOverlordGame("game Id", stubAiCreator, [])();
   expect(game).toBeRight();
   expect(game).toSubsetEqualRight({ aiOverlord: { introduction: "Hi" } });
 });
