de1719210258c817513d07e71199a18becd13e91
FooDog <andyriffic@gmail.com>
2023-07-16 16:02:37 +1000

    Update link to minigame to use next Link

--- src/components/LinkToMiniGame.tsx
+++ src/components/LinkToMiniGame.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import Link from "next/link";
 import { getWhosThatUrl } from "../utils/url";
 
-const FancyLink = styled.a``;
+const FancyLink = styled(Link)``;
 
 export function LinkToMiniGame() {
   return (
