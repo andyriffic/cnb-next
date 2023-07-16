a8ef3b758e129f242141fb54ac3ed6dadf3135e0
FooDog <andyriffic@gmail.com>
2023-07-16 16:05:04 +1000

    Don't deploy if typecheck fails

--- auto/deploy-test.sh
+++ auto/deploy-test.sh
@@ -1,3 +1,9 @@
+
+# exit when any command fails
+set -e
+
+npm run typecheck
+
 DYNAMO_DB_ACCESS_KEY=$(AWS_PROFILE=cnb-next-copilot aws ssm get-parameter --name "/copilot/cnb-next/test/secrets/DYNAMO_DB_ACCESS_KEY" --with-decryption --query "Parameter.Value" --output text) \
 DYNAMO_DB_ACCESS_KEY_SECRET=$(AWS_PROFILE=cnb-next-copilot aws ssm get-parameter --name "/copilot/cnb-next/test/secrets/DYNAMO_DB_ACCESS_KEY_SECRET" --with-decryption --query "Parameter.Value" --output text) \
 OPEN_AI_API_KEY=$(AWS_PROFILE=cnb-next-copilot aws ssm get-parameter --name "/copilot/cnb-next/test/secrets/OPEN_AI_API_KEY" --with-decryption --query "Parameter.Value" --output text) \
