39f9a13d5a08aaf50fd2d2b1b2abae27684383a9
FooDog <andyriffic@gmail.com>
2023-07-17 20:48:29 +1000

    Save pacmans location at end of game

--- src/components/pacman/hooks/useSyncData.ts
+++ src/components/pacman/hooks/useSyncData.ts
@@ -1,3 +1,4 @@
+import { SETTINGS_PLAYER_ID } from "../../../constants";
 import { updatePlayerDetails } from "../../../utils/api";
 import { useDoOnce } from "../../hooks/useDoOnce";
 import { PacManUiState } from "./usePacman/reducer";
@@ -13,6 +14,13 @@ export function useSyncData(uiState: PacManUiState, disabled: boolean) {
           hasPowerPill: pacPlayer.powerPill,
         },
       });
+    });
+    updatePlayerDetails(SETTINGS_PLAYER_ID, {
+      pacmanDetails: {
+        index: uiState.pacMan.pathIndex,
+        hasPowerPill: false,
+        jailTurnsRemaining: 0,
+      },
     });
   }, uiState.status === "game-over" && !disabled);
 }
