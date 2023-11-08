d239e752e27c8be8efbdbd9c1a9488076c076941
FooDog <andyriffic@gmail.com>
2023-11-09 10:51:51 +1100

    Update Zombie RRun background

--- src/components/zombie-run/ZombieRunningTrack.tsx
+++ src/components/zombie-run/ZombieRunningTrack.tsx
@@ -21,7 +21,7 @@ const ZombieBackground = styled.div`
   width: 100vw;
   margin: 0 auto;
   box-sizing: border-box;
-  background: url("/images/zombie-background-night.png") no-repeat bottom center;
+  background: url("/images/zombie-background-dusk.png") no-repeat bottom center;
   background-size: 100% 100%;
 `;
 
