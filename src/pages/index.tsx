52159db86b747dabd5bdce7bb247cdde435e625c
FooDog <andyriffic@gmail.com>
2023-12-04 18:35:48 +1100

    Add Appear animation to mascot image on root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
 import finxMascotImage from "../assets/finx-mascot.png";
+import { Appear } from "../components/animations/Appear";
 
 const Container = styled.div`
   display: flex;
@@ -20,10 +21,12 @@ const Home: NextPage = () => {
       <Head>
         <title>Finx Rocks!</title>
       </Head>
-      <Image
-        src={finxMascotImage}
-        alt="Finx Rocks mascot of a rock smiling and using a laptop"
-      />
+      <Appear animation="flip-in">
+        <Image
+          src={finxMascotImage}
+          alt="Finx Rocks mascot of a rock smiling and using a laptop"
+        />
+      </Appear>
       <Link href="/join">Play a game</Link>
     </Container>
   );
