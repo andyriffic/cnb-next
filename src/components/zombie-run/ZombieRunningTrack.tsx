78a699aba72142a8f6e250819514e9ca356345e7
FooDog <andyriffic@gmail.com>
2024-03-28 17:28:37 +1100

    Update zombie run background to daytime

--- src/components/zombie-run/ZombieRunningTrack.tsx
+++ src/components/zombie-run/ZombieRunningTrack.tsx
@@ -23,7 +23,7 @@ const ZombieBackground = styled.div`
   width: 100vw;
   margin: 0 auto;
   box-sizing: border-box;
-  background: url("/images/zombie-background-dusk.png") no-repeat bottom center;
+  background: url("/images/zombie-background-day.png") no-repeat bottom center;
   background-size: 100% 100%;
 `;
 
