e3f432e27148f7a7f84e5b301ce9fbce17acae05
FooDog <andyriffic@gmail.com>
2024-05-21 18:44:46 +1000

    Update to sensible card weight for bomb card

--- src/services/migrated/gas-out/index.ts
+++ src/services/migrated/gas-out/index.ts
@@ -849,7 +849,7 @@ function getRandomCardType(
         ]
       : [
           { weight: 1, item: "skip" },
-          { weight: 10, item: "bomb" },
+          { weight: 3, item: "bomb" },
           { weight: 2, item: "risky" },
           { weight: 3, item: "reverse" },
           { weight: 14, item: "press" },
