bd6ece4232f208a98ace69ff13b37ab947714c26
FooDog <andyriffic@gmail.com>
2026-05-11 22:09:17 +0530

    Update to indian style cinby on intro screens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
-import finxMascotImage from "../assets/finx-mascot-space.png";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+import finxMascotImage from "../assets/cinby-india.png";
 import { Appear } from "../components/animations/Appear";
 import { spinAnimation } from "../components/animations/keyframes/spinAnimations";
+import { useSocketIo } from "../providers/SocketIoProvider";
 
 const Container = styled.div`
   display: flex;
@@ -23,17 +26,20 @@ const Space = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: black;
-  background-image: url("/images/space-background-01.jpg");
+  background-image: url("/images/cinby-indian-background.png");
   background-size: 100% 100%;
 `;
 
 const StyledLink = styled(Link)`
   color: white;
+  background-color: brown;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
 `;
 
 const PositionContainer = styled.div`
   position: absolute;
-  top: 50%;
+  bottom: -10%;
   left: 50%;
   transform: translate(-50%, -50%);
 `;
@@ -44,6 +50,21 @@ const SpinContainer = styled.div`
 `;
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const { groupJoin } = useSocketIo();
+
+  const startNewGame = useCallback(
+    (team?: string) => {
+      console.log("Creating New Player Group...");
+
+      groupJoin.createPlayerGroup((groupId) => {
+        console.log("Group Created", groupId);
+        router.push(`/join/${groupId}${team ? `?team=${team}` : ""}`);
+      });
+    },
+    [groupJoin, router]
+  );
+
   return (
     <Container>
       <Head>
@@ -51,13 +72,13 @@ const Home: NextPage = () => {
       </Head>
       <Space>
         <PositionContainer>
-          <SpinContainer>
-            <Image
-              src={finxMascotImage}
-              alt="Finx Rocks mascot of a rock smiling and using a laptop"
-              width={300}
-            />
-          </SpinContainer>
+          {/* <SpinContainer> */}
+          <Image
+            src={finxMascotImage}
+            alt="Finx Rocks mascot of a rock smiling and using a laptop"
+            width={200}
+          />
+          {/* </SpinContainer> */}
           <StyledLink href="/join">Play a game</StyledLink>
         </PositionContainer>
       </Space>
