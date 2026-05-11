9372c5c1018665bc1fb9d3e36f3e2b706173c1da
FooDog <andyriffic@gmail.com>
2026-05-11 20:59:42 +0530

    Change mini-game to Pacman

--- src/services/save-game-moves/index.ts
+++ src/services/save-game-moves/index.ts
@@ -1,4 +1,4 @@
-import { savePlayersGameMoves } from "./saveGameMovesZombieRun";
+import { savePlayersGameMoves } from "./saveGameMovesPacman";
 
 //Update this to the current mini-game so users get the correct points allocation and game behaviour
 export default savePlayersGameMoves;
