a1a2acfd16f1e606292e4237f3e7826332d77cd1
FooDog <andyriffic@gmail.com>
2026-07-06 09:59:41 +1000

    Corgi updates again

--- src/themes/corgi/JoinScreenDecoration.tsx
+++ src/themes/corgi/JoinScreenDecoration.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import styled from "styled-components";
 import backgroundImage from "./corgi-decoration.png";
+import dirtImage from "./dirt.png";
 
 const Container = styled.div`
   position: relative;
+  background: url(${dirtImage.src}) repeat-x bottom left;
+  padding-bottom: 40px;
+  overflow: hidden;
 `;
 
 const BackgroundImage = styled(Image)`
   position: relative;
   display: block;
-  width: 100%;
+  max-width: 400px;
+  height: 170px;
 `;
 
 export function JoinScreenDecoration(): JSX.Element {
